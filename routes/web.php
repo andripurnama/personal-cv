0833b928b621a80737909a8e638449a39b4a2822
andripurnama <andri.purnama995@hotmail.com>
2024-08-20 06:44:11 +0700

    feat: added url for download cv

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\DownloadCvController;
 use App\Http\Controllers\Userprofile;
+use App\Http\Controllers\UserProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,10 +16,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [Userprofile::class, 'index'])->name('index');
-Route::get('/user/{id}', [Userprofile::class, 'view'])->name('user.profile.view');
-Route::get('/create', [Userprofile::class, 'create'])->name('user.profile.create');
-Route::post('/store', [Userprofile::class, 'store'])->name('store');
-Route::get('/edit/{id}', [Userprofile::class, 'edit'])->name('edit');
-Route::post('/update', [Userprofile::class, 'update'])->name('update');
-Route::post('/destroy/{id}', [Userprofile::class, 'destroy'])->name('destroy');
+Route::get('/', [UserProfileController::class, 'index'])->name('index');
+Route::get('/user/{id}', [UserProfileController::class, 'view'])->name('user.profile.view');
+Route::get('/create', [UserProfileController::class, 'create'])->name('user.profile.create');
+Route::post('/store', [UserProfileController::class, 'store'])->name('store');
+Route::get('/edit/{id}', [UserProfileController::class, 'edit'])->name('edit');
+Route::post('/update', [UserProfileController::class, 'update'])->name('update');
+Route::post('/destroy/{id}', [UserProfileController::class, 'destroy'])->name('destroy');
+Route::get('/download-cv/{id}',[DownloadCvController::class,'download'])->name('download.cv');
